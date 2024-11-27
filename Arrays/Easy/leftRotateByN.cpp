74a2fb6b2231f7cc8a170b6ce58b071457572c89
ShreeniwasKolagal <kolagalshreeniwas@gmail.com>
2024-11-27 13:17:16 +0530

    Update(Arrays/Easy): Update leftRotateByN.cpp

--- Arrays/Easy/leftRotateByN.cpp
+++ Arrays/Easy/leftRotateByN.cpp
@@ -2,7 +2,11 @@
 #include <vector>
 using namespace std;
 
-void leftRotate(vector<int> &arr, int n, int k)
+/*
+ ? Time Complexity: O(N)
+ ? Space Complexity: O(K)
+*/
+void leftRotate(vector<int> arr, int n, int k)
 {
     vector<int> temp;
 
@@ -20,18 +24,54 @@ void leftRotate(vector<int> &arr, int n, int k)
     {
         arr[i] = temp[i - n + k];
     }
+
+    cout << "After Rotating for " << k << " times: ";
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+/*
+ ? Time Complexity: O(N)
+ ? Space Complexity: O(1)
+*/
+void reverse(vector<int> &arr, int start, int end)
+{
+    while (start <= end)
+    {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++, end--;
+    }
+}
+
+void leftRotate1(vector<int> arr, int n, int k)
+{
+    k = k % n;
+
+    // Reverse first k elements
+    reverse(arr, 0, k - 1);
+    // Reverse rest of the elements
+    reverse(arr, k, n - 1);
+    // Reverse entire array
+    reverse(arr, 0, n - 1);
+
+    cout << "After Rotating for " << k << " times: ";
+    for (auto i : arr)
+    {
+        cout << i << " ";
+    }
 }
 
 int main()
 {
     vector<int> arr = {1, 2, 3, 4, 5, 6, 7};
     int n = arr.size();
-    int k = 9;
+    int k = 2;
 
     leftRotate(arr, n, k);
-    cout << "After Rotating for " << k << " times: ";
-    for (auto i : arr)
-    {
-        cout << i << " ";
-    }
+    leftRotate1(arr, n, k);
 }
