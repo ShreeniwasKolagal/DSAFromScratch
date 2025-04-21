fc236d53abc139e62a70ac550b8c77ffcdbffd98
ShreeniwasKolagal <kolagalshreeniwas@gmail.com>
2025-04-21 11:33:02 +0530

    feat(Graphs/Learning): add adjacencyMatrix.cpp

---
+++ Graphs/Learning/adjacencyMatrix.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int v,e; // v -> vertices, e -> edges
+    cin >> v >> e;
+
+    vector<vector<int>> adj(v+1, vector<int>(v+1, 0));
+
+    for(int i=0; i<e; i++)
+    {
+        int v1, v2;
+        cin >> v1 >> v2;
+        adj[v1][v2] = adj[v2][v1] = 1;
+    }
+
+    for(int i=0; i<=v; i++)
+    {
+        for(int j = 0; j<=v; j++)
+        {
+            cout << adj[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
